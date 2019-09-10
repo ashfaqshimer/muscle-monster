41da67b6ebd87ae840f5c8664b1d27970c1a4412
ashfaqshimer <shimer.ahamed@gmail.com>
2019-09-11 01:38:55 +0530

    Add configuration to serve up public assets when in production mode

--- src/server.js
+++ src/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// Check to see if in production, add a handler to serve up js assets
+if (process.env.ENVIRONMENT === 'prod') {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+		);
+	});
+}
+
 // Server configuration
 const PORT = process.env.PORT;
 
