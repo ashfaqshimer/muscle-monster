be77dc9947962562fa2e0d0bdd040cdb4b15592c
ashfaqshimer <shimer.ahamed@gmail.com>
2019-09-16 21:50:00 +0530

    Integrate stripe component. Optimize production build to hide redux logger

--- client/src/pages/CheckoutPage/CheckoutPage.js
+++ client/src/pages/CheckoutPage/CheckoutPage.js
@@ -8,6 +8,7 @@ import {
 } from '../../redux/cart/cartSelectors';
 import './CheckoutPage.scss';
 import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
+import StripeButton from '../../components/StripeButton/StripeButton';
 
 const CheckoutPage = ({ cartItems, total }) => {
 	return (
@@ -33,6 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => {
 				<CheckoutItem key={cartItem.id} cartItem={cartItem} />
 			))}
 			<div className='total'>TOTAL: ${total}</div>
+			<div className='test-warning'>
+				*Please use the following test credit card for payments*
+				<br />
+				4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+			</div>
+			<StripeButton price={total} />
 		</div>
 	);
 };
