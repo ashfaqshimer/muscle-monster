ff35063de3318f369c9f8bf3dbdbf737d33824f6
ashfaqshimer <shimer.ahamed@gmail.com>
2020-02-10 23:22:39 +0530

    Initialize new project

---
+++ server.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+// Security libraries
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
+// Local imports
+const connectDb = require('./config/db');
+const colors = require('colors');
+const errorHandler = require('./middleware/errorHandler');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDb();
+
+// Route files
+// const bootcamps = require('./routes/bootcamps');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+// File uploading
+app.use(fileupload());
+
+// ----------------------------------SECURITY MIDDLEWARE-----------------------------------------------------------
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({ windowMs: 10 * 60 * 1000, max: 100 }); // 10 mins
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// ----------------------------------SERVER LOGIC-----------------------------------------------------------
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routers
+// app.use('/api/v1/bootcamps', bootcamps);
+
+// Error handler
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+			.bold
+	)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.error(`Error: ${err.message}`.red);
+	// Close server and exit process
+	server.close(() => process.exit(1));
+});
