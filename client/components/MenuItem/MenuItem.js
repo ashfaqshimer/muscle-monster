d0cfcc89df4fcf9a888a7d8676154e74dfb5b75c
ashfaqshimer <shimer.ahamed@gmail.com>
2020-05-09 22:38:35 +0530

    Setup routing from shop page

--- client/components/MenuItem/MenuItem.js
+++ client/components/MenuItem/MenuItem.js
@@ -1,23 +1,25 @@
-import React from 'react';
 import { withRouter } from 'next/router';
+import Link from 'next/link';
 
 import './MenuItem.scss';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
 	return (
-		<div
-			className='MenuItem'
-			onClick={() => history.push(`${match.url}${linkUrl}`)}
-		>
+		<Link href={'/shop/[category]'} as={`/shop/${title}`}>
 			<div
-				className='background-image'
-				style={{ background: `url(${imageUrl})` }}
-			/>
-			<div className='content'>
-				<h1 className='title'>{title.toUpperCase()}</h1>
-				<span className='subtitle'>Shop Now</span>
+				className='MenuItem'
+				// onClick={() => history.push(`${match.url}${linkUrl}`)}
+			>
+				<div
+					className='background-image'
+					style={{ background: `url(${imageUrl})` }}
+				/>
+				<div className='content'>
+					<h1 className='title'>{title.toUpperCase()}</h1>
+					<span className='subtitle'>Shop Now</span>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
