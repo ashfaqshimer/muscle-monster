76479bc2180d291ea6c45d2b56358465a32e242a
ashfaqshimer <shimer.ahamed@gmail.com>
2020-06-28 19:47:01 +0530

    Integrate product page components

--- client/pages/products/[id]/index.js
+++ client/pages/products/[id]/index.js
@@ -1,29 +1,16 @@
-// import ProductSummary from '../../../components/Product/ProductSummary';
-// import ProductAttributes from '../components/Product/ProductAttributes';
+import ProductSummary from '../../../components/Product/ProductSummary';
+import ProductAttributes from '../../../components/Product/ProductAttributes';
 
 import { getProducts, getProductById } from '../../../actions/shop';
 
 const ProductPage = ({ product, user }) => {
-	console.log(product);
 	return (
 		<div>
-			{/* <ProductSummary user={user} {...product} />
-			<ProductAttributes user={user} {...product} /> */}
-			Product page
+			<ProductSummary user={user} {...product} />
+			<ProductAttributes user={user} {...product} />
 		</div>
 	);
 };
-
-// Product.getInitialProps = async ({ query: { _id } }) => {
-// 	try {
-// 		const url = `${baseUrl}/api/product`;
-// 		const payload = { params: { _id } };
-// 		const response = await axios.get(url, payload);
-
-// 	} catch (err) {
-// 		console.log(err);
-// 	}
-// };
 
 export async function getStaticProps({ params }) {
 	// Write an action to fetch the product by slug
